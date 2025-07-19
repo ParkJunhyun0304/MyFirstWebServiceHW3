dc21005b6d89fa86df202f90514aaedeb2f466c7
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-19 16:41:47 +0900

    [Feat] VO 생성

--- src/main/java/org/example/WebServiceHW3/vo/FurnitureVO.java
+++ src/main/java/org/example/WebServiceHW3/vo/FurnitureVO.java
@@ -1,4 +1,21 @@
 package org.example.WebServiceHW3.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class FurnitureVO {
+    int id;
+    String name;
+    int price;
+    int category;
+    int width;
+    int depth;
+    int height;
 }
