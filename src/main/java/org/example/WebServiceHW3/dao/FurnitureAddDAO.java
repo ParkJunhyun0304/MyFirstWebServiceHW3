bb5f533f1fbda7f0d2d5a4dca8082ebc0f19fc13
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-07-19 16:43:38 +0900

    [INIT] DAO 생성

--- src/main/java/org/example/WebServiceHW3/dao/FurnitureAddDAO.java
+++ src/main/java/org/example/WebServiceHW3/dao/FurnitureAddDAO.java
@@ -1,2 +1,12 @@
-package org.example.WebServiceHW3.dao;public class FurnitureAddDAO {
+package org.example.WebServiceHW3.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FurnitureAddDAO {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
 }
